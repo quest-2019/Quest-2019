67f71fbe0a5b7027d14de718e9ca297573df8590
Daniel Min <danieldmin216@gmail.com>
2019-05-25 10:22:44 -0400

    mongoose website

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Quest-2019
 Quest Project 2019
+By Andson Li, Daniel Min, John Baek
---
+++ game.js
@@ -0,0 +1,14 @@
+document.write("This is our amazing game");
+
+var john;
+for (john = 1; john <= 3; john++) { 
+  document.write("<br>");
+  document.write(john);
+  document.write(" johns");
+}
+
+var anderson = 4;
+if (anderson > 3) {
+	document.write("<br>");
+	document.write("Anderson is larger than 3");
+}
